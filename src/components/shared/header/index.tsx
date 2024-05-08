906b5622721fa62528449dd128d24aba57157df3
arsakka <ammar.sakka@agmostudio.com>
2024-05-08 10:05:22 +0800

    add production env file

--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -10,7 +10,7 @@ function Header() {
                 <div className="flex gap-4 items-center justify-between py-2 px-6 m-4 shadow-lg rounded bg-gradient-to-br from-main-white/20 to-main-white/60">
                     <div className="flex gap-2 items-center">
                         <p className="text-main-black font-medium">
-                            OfficeCrafter
+                            OfficeCrafters
                         </p>
                     </div>
                     <div className="flex items-center gap-2">
