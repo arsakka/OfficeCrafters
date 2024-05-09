8f5ee7c40172cec5f91b9fc04afbbaf0ff992d9d
arsakka <ammar.sakka@agmostudio.com>
2024-05-09 09:20:13 +0800

    add services list contant

---
+++ src/constants/ServicesList.tsx
@@ -0,0 +1,73 @@
+import OfficeSpaceImage from "@/public/images/offices/office_space_leasing.jpg";
+import OfficeDesignImage from "@/public/images/offices/office_design_and_build_out.jpg";
+import OfficeFurnitureImage from "@/public/images/offices/office_furniture_and_equipment.jpg";
+import OfficeManagementImage from "@/public/images/offices/office_management_solutions.jpg";
+import VirtualOfficeImage from "@/public/images/offices/virtual_office_services.jpg";
+
+export const ServicesList = [
+    {
+        name: "Office Space Leasing",
+        image: OfficeSpaceImage,
+        description:
+            "We offer a comprehensive range of office spaces to suit your specific needs, from private offices and coworking spaces to entire floors in prestigious commercial buildings. Our team works tirelessly to find the perfect space that aligns with your budget, location preferences, and business requirements.",
+        includes: [
+            {
+                label: "",
+                icon: "",
+            },
+        ],
+        url: "/office_space_leasing",
+    },
+    {
+        name: "Office Design and Build-out",
+        image: OfficeDesignImage,
+        description:
+            "Transform your vision into reality with our office design and build-out services. Our team of experienced designers and contractors will collaborate closely with you to create a customized workspace that reflects your brand identity, enhances productivity, and fosters collaboration among your team members.",
+        includes: [
+            {
+                label: "",
+                icon: "",
+            },
+        ],
+        url: "/office_design_and_build_out",
+    },
+    {
+        name: "Office Furniture and Equipment",
+        image: OfficeFurnitureImage,
+        description:
+            "Furnishing your office with ergonomic furniture and state-of-the-art equipment is essential for creating a comfortable and efficient work environment. We partner with leading manufacturers to provide you with a wide selection of high-quality furniture, technology solutions, and office supplies that meet your functional and aesthetic preferences.",
+        includes: [
+            {
+                label: "",
+                icon: "",
+            },
+        ],
+        url: "/office_furniture_and_equipment",
+    },
+    {
+        name: "Office Management Solutions",
+        image: OfficeManagementImage,
+        description:
+            "Streamline your day-to-day operations with our office management solutions. From facility maintenance and janitorial services to mail handling and receptionist support, we offer a range of services designed to optimize the efficiency and functionality of your workspace, allowing you to focus on what matters most – growing your business.",
+        includes: [
+            {
+                label: "",
+                icon: "",
+            },
+        ],
+        url: "/office_management_solutions",
+    },
+    {
+        name: "Virtual Office Services",
+        image: VirtualOfficeImage,
+        description:
+            "Establish a professional presence without the need for a physical office space with our virtual office services. Benefit from prestigious business addresses, mail handling services, and access to meeting rooms and coworking spaces on an as-needed basis, giving you the flexibility to work remotely while maintaining a professional image.",
+        includes: [
+            {
+                label: "",
+                icon: "",
+            },
+        ],
+        url: "/virtual_office_services",
+    },
+];
