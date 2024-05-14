6f0723e3deea909e499265eed20339d55679b7c9
arsakka <ammar.sakka@agmostudio.com>
2024-05-14 09:06:02 +0800

    change the form style and add footer

--- src/components/home/contact/components/ContactForm.tsx
+++ src/components/home/contact/components/ContactForm.tsx
@@ -1,36 +1,83 @@
+"use client";
 import { GreenButton } from "@/src/constants/styles";
 import React from "react";
 
 function ContactForm() {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    };
+
     return (
         <React.Fragment>
-            <div className="grid gap-4">
+            <form className="grid gap-4" onSubmit={handleSubmit}>
                 <div className="grid gap-4">
-                    <label htmlFor="" className="text-lg text-main-black font-medium capitalize">full name</label>
-                    <input type="text" className="py-2 px-4 bg-main-white rounded text-main-black" />
+                    <label
+                        htmlFor="full_name"
+                        className="text-lg text-main-black font-medium capitalize drop-shadow"
+                    >
+                        full name
+                    </label>
+                    <input
+                        type="text"
+                        className="py-2 px-4 bg-main-white rounded text-main-black placeholder:capitalize resize-none shadow"
+                        placeholder="full name"
+                        name="full_name"
+                        id="full_name"
+                    />
                 </div>
 
                 <div className="grid gap-4">
-                    <label htmlFor="" className="text-lg text-main-black font-medium capitalize">email address</label>
-                    <input type="text" className="py-2 px-4 bg-main-white rounded text-main-black" />
+                    <label
+                        htmlFor="email_address"
+                        className="text-lg text-main-black font-medium capitalize drop-shadow"
+                    >
+                        email address
+                    </label>
+                    <input
+                        type="text"
+                        className="py-2 px-4 bg-main-white rounded text-main-black placeholder:capitalize resize-none shadow"
+                        placeholder="email address"
+                        name="email_address"
+                        id="email_address"
+                    />
                 </div>
 
                 <div className="grid gap-4">
-                    <label htmlFor="" className="text-lg text-main-black font-medium capitalize">business name</label>
-                    <input type="text" className="py-2 px-4 bg-main-white rounded text-main-black" />
+                    <label
+                        htmlFor="business_name"
+                        className="text-lg text-main-black font-medium capitalize drop-shadow"
+                    >
+                        business name
+                    </label>
+                    <input
+                        type="text"
+                        className="py-2 px-4 bg-main-white rounded text-main-black placeholder:capitalize resize-none shadow"
+                        placeholder="business name"
+                        name="business_name"
+                        id="business_name"
+                    />
                 </div>
 
                 <div className="grid gap-4">
-                    <label htmlFor="" className="text-lg text-main-black font-medium capitalize">message</label>
-                    <textarea rows={5} className="py-2 px-4 bg-main-white rounded text-main-black" />
+                    <label
+                        htmlFor="message"
+                        className="text-lg text-main-black font-medium capitalize drop-shadow"
+                    >
+                        message
+                    </label>
+                    <textarea
+                        rows={5}
+                        className="py-2 px-4 bg-main-white rounded text-main-black placeholder:capitalize resize-none shadow"
+                        placeholder="message"
+                        name="message"
+                        id="message"
+                    />
                 </div>
 
                 <div className="flex justify-end">
-                    <button className={GreenButton}>
-                        Submit
-                    </button>
+                    <button className={GreenButton}>Submit</button>
                 </div>
-            </div>
+            </form>
         </React.Fragment>
     );
 }
--- src/components/shared/footer/index.tsx
+++ src/components/shared/footer/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Footer() {
-    return <div>Footer</div>;
+    const date = new Date();
+
+    return (
+        <React.Fragment>
+            <footer className="bg-main-gray py-4 px-8">
+                <p className="text-main-white text-center text-sm">Copyright &copy; {date.getFullYear()}, All rights reserved.</p>
+            </footer>
+        </React.Fragment>
+    );
 }
 
 export default Footer;
